de9ca164e81d9d8d1c00548133b6f0dbd0f23528
Nima-Chenarani-1386 <nima.chenarani.86@gmail.com>
2025-02-28 21:02:35 -0800

    ...
    ...

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // definde variables 
 const theme_toggle_button = document.getElementById('theme__toggle'); 
 const body = document.body;
+const nav = document.getElementById('navbar'); 
 
 // Click functions
 theme_toggle_button.addEventListener('click', () => {
