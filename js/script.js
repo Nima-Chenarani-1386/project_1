c08db1feacee82303450adf7585a2b25ed99fa56
Nima-Chenarani-1386 <nima.chenarani.86@gmail.com>
2025-03-01 16:11:39 -0800

    Update index.html

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 // definde variables 
-const theme_toggle_button = document.getElementById('theme__toggle'); 
+const theme_toggle_button = document.getElementById('theme__toggle');
 const body = document.body;
-const nav = document.getElementById('navbar'); 
+// const nav = document.getElementById('navbar'); 
 
 // Click functions
 theme_toggle_button.addEventListener('click', () => {
     document.body.classList.toggle('dark__mode')
+});
+
+window.addEventListener('scroll', () => {
+    if (window.scroll >= 100) {
+        document.querySelector('#navbar').classList.add('fixed');
+    } else {
+        document.querySelector('#navbar').classList.remove('fixed');
+    }
 }); 
